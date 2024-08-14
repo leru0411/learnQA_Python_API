cc60486d8da04d61e8524c4ceb1448267132373e
Valeriya <leru0411@mail.ru>
2024-08-14 22:12:20 +0300

    get_text

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_500")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 
 print(response.text)
